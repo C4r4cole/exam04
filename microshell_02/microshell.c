b3c6b4e786ed09b20edfe1a1f845c859a5d5c774
C4r4cole <c4r4cole.chevalerie@gmail.com>
2025-12-08 21:42:59 +0100

    3 times

---
+++ microshell_02/microshell.c
@@ -0,0 +1,109 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   microshell.c                                       :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: fmoulin <fmoulin@student.42.fr>            +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2025/12/08 21:06:01 by fmoulin           #+#    #+#             */
+/*   Updated: 2025/12/08 21:42:22 by fmoulin          ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "microshell.h"
+
+int	ft_error(char *msg)
+{
+	write(2, msg, strlen(msg));
+	return (1);
+}
+
+int	exec_cd(char **argv, int size)
+{
+	if (size != 2)
+		return (ft_error("error: cd: bad arguments\n"));
+	if (chdir(argv[1]) == -1)
+	{
+		ft_error("error: cd: cannot change directory to ");
+		ft_error(argv[1]);
+		ft_error("\n");
+		return (1);
+	}
+	return (0);
+}
+
+int	exec_cmd(char **argv, int size, char **envp, int *saved_stdin)
+{
+	int	fd[2];
+	int	pid;
+	int	pipe_needed;
+	
+	pipe_needed = (argv[size] && strcmp(argv[size], "|") == 0);
+	argv[size] = NULL;
+	if (pipe_needed && pipe(fd) == -1)
+		return (ft_error("error: fatal"));
+	pid = fork();
+	if (pid < 0)
+		return (ft_error("error: fatal"));
+	if (pid == 0)
+	{
+		dup2(*saved_stdin, 0);
+		close(*saved_stdin);
+		if (pipe_needed)
+		{
+			dup2(fd[1], 1);
+			close(fd[0]);
+			close(fd[1]);
+		}
+		execve(argv[0], argv, envp);
+		ft_error("error: cannot execute ");
+		ft_error(argv[0]);
+		ft_error("\n");
+		exit(1);
+	}
+	waitpid(pid, NULL, 0);
+	if (pipe_needed)
+	{
+		dup2(fd[0], *saved_stdin);
+		close(fd[0]);
+		close(fd[1]);
+	}
+	else
+	{
+		dup2(0, *saved_stdin);
+	}
+	return (0);
+}
+
+int	main(int argc, char **argv, char **envp)
+{
+	(void)argc;
+	int	i;
+	int	saved_stdin;
+	int	size;
+	char **cmd;
+
+	i = 1;
+	saved_stdin = dup(0);
+	while (argv[i])
+	{
+		cmd = &argv[i];
+		size = 0;
+		while(argv[i] && strcmp(argv[i], "|") && strcmp(argv[i], ";"))
+		{
+			i++;
+			size++;
+		}
+		if (size)
+		{
+			if (strcmp(cmd[0], "cd") == 0)
+				exec_cd(cmd, size);
+			else
+				exec_cmd(cmd, size, envp, &saved_stdin);
+		}
+		if (argv[i])
+			i++;
+	}
+	close(saved_stdin);
+	return (0);
+}
---
+++ microshell_02/microshell.h
@@ -0,0 +1,21 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   microshell.h                                       :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: fmoulin <fmoulin@student.42.fr>            +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2025/12/08 21:06:06 by fmoulin           #+#    #+#             */
+/*   Updated: 2025/12/08 21:09:00 by fmoulin          ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef MICROSHELL_H
+# define MICROSHELL_H
+
+# include <unistd.h>
+# include <stdlib.h>
+# include <string.h>
+# include <sys/wait.h>
+
+#endif
