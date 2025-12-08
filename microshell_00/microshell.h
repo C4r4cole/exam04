ee26af3232990800d27a48180a3f5c06812fb707
C4r4cole <c4r4cole.chevalerie@gmail.com>
2025-12-08 21:05:49 +0100

    2 times

---
+++ microshell_00/microshell.h
@@ -0,0 +1,21 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   microshell.h                                       :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: fmoulin <fmoulin@student.42.fr>            +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2025/12/08 15:29:40 by fmoulin           #+#    #+#             */
+/*   Updated: 2025/12/08 15:57:08 by fmoulin          ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef MICROSHELL_H
+# define MICROSHELL_H
+
+# include <unistd.h>
+# include <stdlib.h>
+# include <sys/wait.h>
+# include <string.h>
+
+#endif
---
+++ microshell_01/microshell.h
@@ -0,0 +1,21 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   microshell.h                                       :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: fmoulin <fmoulin@student.42.fr>            +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2025/12/08 18:53:11 by fmoulin           #+#    #+#             */
+/*   Updated: 2025/12/08 19:17:38 by fmoulin          ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef MICROSHELL_H
+# define MICROSHELL_H
+
+# include <unistd.h>
+# include <stdlib.h>
+# include <string.h>
+# include <sys/wait.h>
+
+#endif
